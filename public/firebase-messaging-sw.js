715ed89d7336f4c3056739b6f587b691e3f496ce
Jochen Heirman <jochen.heirman@gmail.com>
2025-11-14 15:23:26 +0000

    ok let's continue with setting up the mobile native app, I think we prep

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,27 +1,29 @@
+// This file needs to be in the public folder.
 
-// This file must be in the public directory
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js");
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDyKYdIZpO0WFKtnY6t01qzBIkIVDGCrU",
-  authDomain: "studio-5690519872-e0869.firebaseapp.com",
-  projectId: "studio-5690519872-e0869",
-  storageBucket: "studio-5690519872-e0869.firebasestorage.app",
-  messagingSenderId: "796529432751",
-  appId: "1:796529432751:web:da147b13f407d67aaf9c5a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging(app);
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-
+// Optional: background message handler
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
   // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
