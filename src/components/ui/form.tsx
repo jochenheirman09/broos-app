45c0b53e9ca5670de446718b9db02ec415859168
Jochen Heirman <jochen.heirman@gmail.com>
2025-11-27 11:12:30 +0100

    MVP full working version 10

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -5,17 +6,17 @@ import * as LabelPrimitive from "@radix-ui/react-label"
 import { Slot } from "@radix-ui/react-slot"
 import {
   Controller,
-  FormProvider,
   useFormContext,
   type ControllerProps,
   type FieldPath,
   type FieldValues,
 } from "react-hook-form"
+import FormProvider from "react-hook-form/dist/context";
 
 import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
 
-const Form = FormProvider
+const Form = FormProvider;
 
 type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
