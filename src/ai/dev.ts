715df401413dd2b2a2d8860e1da123f8505d8807
Jochen Heirman <jochen.heirman@gmail.com>
2025-11-28 09:49:08 +0100

    MVP full working version Google Secret 5

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-
-"use server";
-// Flows will be imported for their side effects in this file.
-import './flows/buddy-flow';
-import './flows/team-analysis-flow';
-import './flows/club-analysis-flow';
-import './flows/ingest-flow';
-import './flows/notification-flow';
